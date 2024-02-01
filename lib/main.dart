fa5a02121dbdd71298670c764e60b4958a28a2c7
Max28944 <maxthanakorn28944@gmail.com>
2024-02-01 11:32:59 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_node_store/themes/styles.dart';
+import 'package:flutter_node_store/app_route.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// กำหนดตัวแปร initialRoute ให้กับ MaterialApp
+var initialRoute;
+
+void main() async{
+ // ต้องเรียกใช้ WidgetsFlutterBinding.ensureInitialized()
+  // เพื่อให้สามารถเรียกใช้ SharedPreferences ได้
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // สร้างตัวแปร prefs เพื่อเรียกใช้ SharedPreferences
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  // ตรวจสอบว่าเคยแสดง Intro แล้วหรือยัง 
+  if(prefs.getBool('welcomeStatus') == true) {
+    // ถ้าเคยแสดง Intro แล้ว ให้ไปยังหน้า Login
+    initialRoute = AppRouter.login;
+  } else {
+    // ถ้ายังไม่เคยแสดง Intro ให้ไปยังหน้า Welcome
+    initialRoute = AppRouter.welcome;
+  }
+
+
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: AppTheme.lightTheme,
+    //theme: AppTheme.darkTheme,
+    initialRoute:initialRoute,
+    routes: AppRouter.routes,
+  );
+  }
+}
